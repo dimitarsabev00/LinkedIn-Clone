a61c49350d831c17c670a8562d7745cf2d62677c
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-31 20:30:25 +0300

    Added Layout Structure For Home Screen

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,12 +1,37 @@
+import styled from "styled-components";
 import { Header } from "../../components";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div>Home</div>
+      <Container>
+        <Layout>
+          {/* <LeftSide /> */}
+          {/* <Main /> */}
+          {/* <RightSide /> */}
+        </Layout>
+      </Container>
     </>
   );
 };
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
 
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+  column-gap: 25px;
+  row-gap: 25px;
+  /* grid-template-row: auto; */
+  margin: 25px 0;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
 export default Home;

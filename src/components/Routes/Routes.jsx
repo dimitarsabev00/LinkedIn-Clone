acdaa6bde402e6e11ee08d1aa37fb41d1765b4d6
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-31 09:28:42 +0300

    Create Home Screen & Added Header Component In Home Screen

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router";
+import Home from "../../screens/Home";
 import Login from "../../screens/Login";
 
 const RoutesComp = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={} /> */}
+      <Route path="/feed" element={<Home />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );

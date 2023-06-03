a9db44afd7a7d12f9a2d424c1936d61d323a01bd
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-06-03 17:44:00 +0300

    Move ShareBox Container From Main Component In ShareBox Component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from "./Header";
 export { default as LeftSide } from "./LeftSide";
 export { default as RightSide } from "./RightSide";
 export { default as Main } from "./Main";
+export { default as ShareBox } from "./ShareBox";

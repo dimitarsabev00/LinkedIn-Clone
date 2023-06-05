44d11246f9fcaf2d1e886bd1c422b82c764ee84d
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-06-05 22:17:05 +0300

    Added Redux Store With configureStore From Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes } from "./components";
 
 const App = () => {
+  const testState = useSelector(({ generalSlice }) => generalSlice.testState);
+  console.log(testState);
   return (
     <Router>
       <Routes />

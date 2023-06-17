47f782122da5209f7a428b69e0a5edabe607187a
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-06-17 13:19:11 +0300

    Fix Bug With serializableCheck In Redux Store

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,10 @@ const store = configureStore({
   reducer: {
     generalSlice,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;

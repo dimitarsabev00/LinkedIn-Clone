d6830c9385963e88299e9a283e4e57a39c9393eb
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-06-24 15:36:38 +0300

    Added ToastContainer In main.jsx File

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App.jsx";
 import "./main.css";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
+    <ToastContainer />
   </Provider>
 );

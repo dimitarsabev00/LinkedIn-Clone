d565c7d9a25aa20a2fa2921fc6ea5343f44d0475
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-06-24 18:25:28 +0300

    Added /* eslint-disable react-hooks/exhaustive-deps */ For App.jsx File

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
